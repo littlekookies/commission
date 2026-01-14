97c717548754fcecf2d757b4cf917ec9e2b3cb00
Andien <littlekookies02@gmail.com>
2026-01-15 00:19:50 +0700

    nambah gambar

--- gallery.js
+++ gallery.js
@@ -19,8 +19,8 @@ const images = [
   'images/gallery/commission result 24.png',
   'images/gallery/commission result 25.png',
   'images/gallery/commission result 26.png',
-  'images/gallery/commission result 26.png',
-  'images/gallery/commission result 27.png'
+  'images/gallery/commission result 27.png',
+  'images/gallery/commission result 28.png'
 ];
 
 const batchSize = 12;
